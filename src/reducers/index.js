b65a4c55884389d2295ba5409c6e085d282986cb
Leslie Thompson <lcrthompson@gmail.com>
2019-04-24 13:55:57 -0700

    completed counter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,12 +13,14 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
       return {
-        ...state
+        ...state,
+        count: state.count + 1
       };
     // Fill in the body of this case
     case DECREMENT:
       return {
-        ...state
+        ...state,
+        count: state.count - 1
       };
     // Fill in the body of this case
     default:
